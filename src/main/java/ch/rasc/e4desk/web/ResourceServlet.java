cc7c50a83f181a1b0f6526925cc2a7e6af0f7d29
Ralph Schaer <ralphschaer@gmail.com>
2013-03-08 14:21:59 +0100

    Compute the etag only once

--- src/main/java/ch/rasc/e4desk/web/ResourceServlet.java
+++ src/main/java/ch/rasc/e4desk/web/ResourceServlet.java
@@ -3,9 +3,12 @@ package ch.rasc.e4desk.web;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.util.DigestUtils;
 
 import ch.ralscha.extdirectspring.util.ExtDirectSpringUtil;
 
@@ -16,15 +19,36 @@ public class ResourceServlet extends HttpServlet {
 	private final byte[] data;
 
 	private final String contentType;
+	
+	private final String etag;
 
 	public ResourceServlet(byte[] data, String contentType) {
 		this.data = data;
 		this.contentType = contentType;
+		etag = "\"0" + DigestUtils.md5DigestAsHex(data) + "\"";
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ExtDirectSpringUtil.handleCacheableResponse(request, response, data, contentType);
+		handleCacheableResponse(request, response);
 	}
+	
+	public void handleCacheableResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String ifNoneMatch = request.getHeader("If-None-Match");
 
+		if (etag.equals(ifNoneMatch)) {
+			response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+			return;
+		}
+
+		response.setContentType(contentType);
+		response.setContentLength(data.length);
+
+		ExtDirectSpringUtil.addCacheHeaders(response, etag, 6);
+
+		@SuppressWarnings("resource")
+		ServletOutputStream out = response.getOutputStream();
+		out.write(data);
+		out.flush();
+	}
 }
