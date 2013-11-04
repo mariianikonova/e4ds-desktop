90d94a93a964fb54c1eacd4a8cf0226a3573dde2
Ralph Schaer <ralphschaer@gmail.com>
2013-11-04 18:20:04 +0100

    Make sure liquibase runs before hibernate

--- src/main/java/ch/rasc/e4desk/config/DataConfig.java
+++ src/main/java/ch/rasc/e4desk/config/DataConfig.java
@@ -12,6 +12,7 @@ import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -35,6 +36,7 @@ public class DataConfig {
 	}
 
 	@Bean
+	@DependsOn("liquibaseBean")
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws NamingException {
 		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
 		emf.setDataSource(dataSource());
