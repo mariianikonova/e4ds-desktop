22b4e1a20229e9f9b70b15c64c151e43f175a669
Ralph Schaer <ralphschaer@gmail.com>
2013-11-04 18:20:16 +0100

    Fix for spring 4.

--- src/main/java/ch/rasc/e4desk/config/ComponentConfig.java
+++ src/main/java/ch/rasc/e4desk/config/ComponentConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @ComponentScan(basePackages = { "ch.ralscha.extdirectspring", "ch.rasc.e4desk.config", "ch.rasc.e4desk.schedule",
 		"ch.rasc.e4desk.security", "ch.rasc.e4desk.service", "ch.rasc.e4desk.web" })
-@PropertySource({ "version.properties" })
+@PropertySource("classpath:/version.properties")
 public class ComponentConfig {
 	// nothing here
 }
