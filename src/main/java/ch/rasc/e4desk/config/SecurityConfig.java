cb149f6b957d841201020e984286e1dd08954c46
Ralph Schaer <ralphschaer@gmail.com>
2013-11-04 18:26:34 +0100

    Method name change to satisfy spring security 3.2.0.rc2

--- src/main/java/ch/rasc/e4desk/config/SecurityConfig.java
+++ src/main/java/ch/rasc/e4desk/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
-	protected void registerAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
 
